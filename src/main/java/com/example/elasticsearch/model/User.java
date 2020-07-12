a90282f6f8356af3310d5df900c12893fc4b11dc
Ibrar <inform2ibrar@gmail.com>
2020-07-12 20:55:55 +0530

    Remove unnecessary code

--- src/main/java/com/example/elasticsearch/model/User.java
+++ src/main/java/com/example/elasticsearch/model/User.java
@@ -16,36 +16,4 @@ public class User {
 	private String id;
 	private String name;
 	private String designation;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDesignation() {
-		return designation;
-	}
-	public void setDesignation(String designation) {
-		this.designation = designation;
-	}
-	
-	public User() {}
-	
-	public User(String id, String name, String designation) {
-		this.id = id;
-		this.name = name;
-		this.designation = designation;
-	}
-	
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", designation=" + designation + "]";
-	}	
 }
